2ff52383d4830a2a2b6383b43c4386573ad603c5
Kyle <kylebonnet@gmail.com>
2015-09-14 11:23:07 -0700

    commit

--- bots/learnpython.py
+++ bots/learnpython.py
@@ -4,10 +4,8 @@ from reddit import cSubredditMonitor
 
 class cLearnPython(cSubredditMonitor):
     def __init__(self):
-        cSubredditMonitor.__init__(self)
-        self.r = praw.Reddit('learnpython monitor by /u/uhkhu')
-        self.login(self.r)
-        self.subreddit = self.r.get_subreddit('learnpython')
+        self.subreddit_name = 'learnpython'
+        cSubredditMonitor.__init__(self, subreddit=self.subreddit_name)
 
 
 
